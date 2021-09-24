b3d9f5b2aa3a79825ebee60965f8d651c65c38a9
Iartyom <artyom11@gmail.com>
2021-09-24 21:00:27 +0300

    1+

---
+++ motors-controll.ino
@@ -0,0 +1,12 @@
+#include <DualTB9051FTGMotorShield.h>
+
+void setup() {
+  // put your setup code here, to run once:
+ Serial.begin(115200);
+  Serial.println("Dual TB9051FTG Motor Shield");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
